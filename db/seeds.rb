1d58024c28d7d4fcc6574b42a546a30ff9bd5b16
Leon <hualiang.tian@gmail.com>
2021-07-10 21:18:46 -0400

    commit tree 82d34ac30d18ec7d2d30ba43a16b57ad515ec13d

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+SchoolClass.create(title: "Math", room_number: "1")
+SchoolClass.create(title: "Literature", room_number: "2")
+SchoolClass.create(title: "History", room_number: "3")
+SchoolClass.create(title: "Physics", room_number: "4")
+SchoolClass.create(title: "Art", room_number: "5")
+
+Student.create(first_name: "Bill", last_name: "Happy")
+Student.create(first_name: "Tom", last_name: "Sunny")
+Student.create(first_name: "Sam", last_name: "Rainny")
+Student.create(first_name: "Zack", last_name: "Silly")
+Student.create(first_name: "Chris", last_name: "Goofy")
